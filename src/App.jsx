40cc9d243de3381b6cc55658ac1ef37e708806bd
JGW-Korea <jgw6372@gmail.com>
2024-09-24 10:32:50 +0900

    feat: Router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import { globalStyle } from '@/styles/globals';
 import { Global, ThemeProvider } from '@emotion/react';
 import { Layout } from '@components/global/Layout';
 import Home from '@pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Layout>
-      <Global styles={globalStyle} />
-      <Home />
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Global styles={globalStyle} />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+
+    // <Layout>
+    //   <Global styles={globalStyle} />
+    //   <Home />
+    // </Layout>
   );
 }
 
