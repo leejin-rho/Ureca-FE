853622b85bbf7f3b82bde48a4b8821a2b015f585
JGW-Korea <jgw6372@gmail.com>
2024-09-24 10:43:28 +0900

    feat: Apply.jsx 생성

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { Layout } from '@components/global/Layout';
 import Home from '@pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Apply from './pages/Apply';
 
 function App() {
   return (
@@ -12,14 +13,10 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/apply" element={<Apply />} />
         </Routes>
       </Layout>
     </BrowserRouter>
-
-    // <Layout>
-    //   <Global styles={globalStyle} />
-    //   <Home />
-    // </Layout>
   );
 }
 
