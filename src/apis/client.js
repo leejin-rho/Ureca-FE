dfdf8e436a6d76dff0ad4615b881b73ac0a68ff1
leejin_rho <tbdpapdl@gmail.com>
2024-10-01 11:01:49 +0900

    feat: FAQ 페이지 모바일 padding 수정 (#14)

--- src/apis/client.js
+++ src/apis/client.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:8080/';
-
 export const client = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.VITE_SERVER_API,
   withCredentials: true,
 });
