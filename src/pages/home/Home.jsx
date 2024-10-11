04dae36b67a897814ee4cc0faf44568c2cbfafe0
leejin_rho <tbdpapdl@gmail.com>
2024-10-11 11:45:26 +0900

    feat: 홈 섹션 애니메이션 keyframe 설정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { css, keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 import { colors } from '@/styles/colors';
@@ -17,6 +17,13 @@ import FAQs from '@components/home/FAQs';
 
 const Home = () => {
   const { sectionRefs } = useScroll();
+  const [isVisible, setIsVisible] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
 
   useEffect(() => {
     window.scrollTo({
@@ -24,32 +31,80 @@ const Home = () => {
     });
   }, []);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const index = sectionRefs.current.indexOf(entry.target);
+          if (entry.isIntersecting && index !== -1) {
+            setIsVisible((prev) => {
+              const updatedVisibility = [...prev];
+              updatedVisibility[index] = true;
+              return updatedVisibility;
+            });
+          }
+        });
+      },
+      { threshold: 0.5 },
+    );
+
+    console.log('sectionRefs:', sectionRefs.current);
+
+    sectionRefs.current.forEach((section) => {
+      if (section) {
+        observer.observe(section);
+      }
+    });
+
+    return () => {
+      sectionRefs.current.forEach((section) => {
+        if (section) {
+          observer.unobserve(section);
+        }
+      });
+    };
+  }, [sectionRefs]);
+
   return (
     <>
       <MainContainer>
         <Subheader />
         <InfoList />
         <SectionList direction="column">
-          <div ref={(el) => (sectionRefs.current[0] = el)}>
+          <Section
+            ref={(el) => (sectionRefs.current[0] = el)}
+            isVisible={isVisible[0]}
+          >
             <Goal />
-          </div>
+          </Section>
 
-          <Flex direction="column" ref={(el) => (sectionRefs.current[1] = el)}>
-            <Benefit />
-            <IntroPJT />
-          </Flex>
+          <Section ref={(el) => (sectionRefs.current[1] = el)} isVisible={true}>
+            <Flex direction="column">
+              <Benefit />
+              <IntroPJT />
+            </Flex>
+          </Section>
 
-          <div ref={(el) => (sectionRefs.current[2] = el)}>
+          <Section
+            ref={(el) => (sectionRefs.current[2] = el)}
+            isVisible={isVisible[2]}
+          >
             <Education />
-          </div>
+          </Section>
 
-          <div ref={(el) => (sectionRefs.current[3] = el)}>
+          <Section
+            ref={(el) => (sectionRefs.current[3] = el)}
+            isVisible={isVisible[3]}
+          >
             <Order />
-          </div>
+          </Section>
 
-          <div ref={(el) => (sectionRefs.current[4] = el)}>
+          <Section
+            ref={(el) => (sectionRefs.current[4] = el)}
+            isVisible={isVisible[4]}
+          >
             <FAQs />
-          </div>
+          </Section>
         </SectionList>
       </MainContainer>
     </>
@@ -84,4 +139,26 @@ const MainContainer = styled.div`
 const SectionList = styled(Flex)`
   width: 100%;
   margin-bottom: 8rem;
+`;
+
+const fadeInUp = keyframes`
+from {
+  opacity: 0;
+  transform: translateY(100px);
+}
+to {
+  opacity: 1;
+  transform: translateY(0);
+}
+`;
+
+const Section = styled.div`
+  opacity: 0;
+  /* transform: translateY(40px); */
+  animation: ${({ isVisible }) =>
+    isVisible
+      ? css`
+          ${fadeInUp} 0.6s ease-out forwards
+        `
+      : 'none'};
 `;
